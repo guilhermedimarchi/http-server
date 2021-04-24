2a2a7a35d185d2b3acbcdd1dcd7de48b2eac983a
guilhermedimarchi <guilherme.dimarchi@outlook.com>
2021-04-24 12:29:55 +0200

    static handler get and head handling

--- src/main/java/com/gui/http/StaticHandler.java
+++ src/main/java/com/gui/http/StaticHandler.java
@@ -1,22 +1,44 @@
 package com.gui.http;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import static com.gui.http.HttpStatus.NOT_FOUND;
-import static com.gui.http.HttpStatus.NOT_IMPLEMENTED;
+import static com.gui.http.HttpStatus.*;
 
 public class StaticHandler {
 
-    public Response handle(Request request) {
+    private String rootPath;
+
+    public StaticHandler(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    public Response handle(Request request) throws IOException {
 
         if(methodNotImplemented(request)) {
-            return new Response(NOT_IMPLEMENTED);
+            return response(NOT_IMPLEMENTED);
         }
-        File f = new File(request.getPath());
-        if(!f.exists())
-            return new Response(NOT_FOUND);
 
-        return null;
+        File f = new File(rootPath + request.getPath());
+        if(!f.exists())
+            return response(NOT_FOUND);
+
+        if("HEAD".equals(request.getMethod())) {
+            return response(OK);
+        } else {
+            byte[] body = Files.readAllBytes(Paths.get(f.getAbsolutePath()));
+            return response(OK, body);
+        }
+    }
+
+    private Response response(HttpStatus status) {
+        return new Response(status);
+    }
+
+    private Response response(HttpStatus status, byte[] body) {
+        return new Response(status, body);
     }
 
     private boolean methodNotImplemented(Request request) {
--- src/test/java/com/gui/http/StaticHandlerTest.java
+++ src/test/java/com/gui/http/StaticHandlerTest.java
@@ -3,20 +3,21 @@ package com.gui.http;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import static com.gui.http.HttpStatus.NOT_FOUND;
-import static com.gui.http.HttpStatus.NOT_IMPLEMENTED;
-
+import static com.gui.http.HttpStatus.*;
 
 public class StaticHandlerTest {
 
     private StaticHandler handler;
+    private static final String rootPath = new File("./src/test/resources").getAbsolutePath();
 
     @BeforeEach
     public void setup() {
-        handler = new StaticHandler();
+        handler = new StaticHandler(rootPath);
     }
 
     @Test
@@ -29,6 +30,19 @@ public class StaticHandlerTest {
     public void givenFileDoesNotExists_shouldRespondNotFound() throws Exception  {
         Response actualResponse = handler.handle(request("GET /non-existent-file.txt HTTP/1.1"));
         Assertions.assertEquals(new Response(NOT_FOUND), actualResponse);
+    }
+
+    @Test
+    public void givenFileExistsAndHeadMethod_shouldRespondOkWithoutBody() throws Exception {
+        Response actualResponse = handler.handle(request("HEAD /index.html HTTP/1.1"));
+        Assertions.assertEquals(new Response(OK), actualResponse);
+    }
+
+    @Test
+    public void givenFileExistsAndGetMethod_shouldRespondOkWithBody() throws Exception {
+        Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1"));
+        byte[] body = Files.readAllBytes(Paths.get(rootPath + "/index.html"));
+        Assertions.assertEquals(new Response(OK, body), actualResponse);
     }
 
     private Request request(String content) throws RequestParseException {
