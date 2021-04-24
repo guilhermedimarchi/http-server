c7975e97ac3dea12ba170061456b4110cc2ff8f9
guilhermedimarchi <guilherme.dimarchi@outlook.com>
2021-04-24 13:57:55 +0200

    include content-type and length headers in get and head responses

--- src/main/java/com/gui/http/StaticHandler.java
+++ src/main/java/com/gui/http/StaticHandler.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 
 import static com.gui.http.HttpStatus.*;
 
@@ -18,27 +19,24 @@ public class StaticHandler {
     public Response handle(Request request) throws IOException {
 
         if(methodNotImplemented(request)) {
-            return response(NOT_IMPLEMENTED);
+            return new Response(NOT_IMPLEMENTED);
         }
 
-        File f = new File(rootPath + request.getPath());
-        if(!f.exists())
-            return response(NOT_FOUND);
+        File file = new File(rootPath + request.getPath());
+        if(!file.exists() || file.isDirectory())
+            return new Response(NOT_FOUND);
+
+        byte[] body = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+        Map<String, String> headers = Map.of(
+                "Content-Type", Files.probeContentType(Paths.get(file.getAbsolutePath())),
+                "Content-Length", "" + body.length
+        );
 
         if("HEAD".equals(request.getMethod())) {
-            return response(OK);
+            return new Response(OK, null, headers);
         } else {
-            byte[] body = Files.readAllBytes(Paths.get(f.getAbsolutePath()));
-            return response(OK, body);
+            return new Response(OK, body, headers);
         }
-    }
-
-    private Response response(HttpStatus status) {
-        return new Response(status);
-    }
-
-    private Response response(HttpStatus status, byte[] body) {
-        return new Response(status, body);
     }
 
     private boolean methodNotImplemented(Request request) {
--- src/test/java/com/gui/http/StaticHandlerTest.java
+++ src/test/java/com/gui/http/StaticHandlerTest.java
@@ -1,51 +1,70 @@
 package com.gui.http;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 
 import static com.gui.http.HttpStatus.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class StaticHandlerTest {
 
     private StaticHandler handler;
     private static final String rootPath = new File("./src/test/resources").getAbsolutePath();
+    private static byte[] body;
+    private static Map<String, String> headers;
 
     @BeforeEach
-    public void setup() {
+    public void setup()  {
         handler = new StaticHandler(rootPath);
     }
 
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        body = Files.readAllBytes(Paths.get(rootPath + "/index.html"));
+        headers = Map.of(
+                "Content-Type", "text/html",
+                "Content-Length", "" + body.length
+        );
+    }
+
     @Test
-    public void givenRequestMethodNotSupported_shouldRespondNotImplemented() throws Exception  {
+    public void whenRequestMethodNotSupported_shouldRespondNotImplemented() throws Exception  {
         Response actualResponse = handler.handle(request("PUT / HTTP/1.1"));
-        Assertions.assertEquals(new Response(NOT_IMPLEMENTED), actualResponse);
+        assertEquals(new Response(NOT_IMPLEMENTED), actualResponse);
     }
 
     @Test
-    public void givenFileDoesNotExists_shouldRespondNotFound() throws Exception  {
+    public void whenFileDoesNotExists_shouldRespondNotFound() throws Exception  {
         Response actualResponse = handler.handle(request("GET /non-existent-file.txt HTTP/1.1"));
-        Assertions.assertEquals(new Response(NOT_FOUND), actualResponse);
+        assertEquals(new Response(NOT_FOUND), actualResponse);
     }
 
     @Test
-    public void givenFileExistsAndHeadMethod_shouldRespondOkWithoutBody() throws Exception {
+    public void whenFileExistsAndHeadMethod_shouldRespondOkWithoutBody() throws Exception {
         Response actualResponse = handler.handle(request("HEAD /index.html HTTP/1.1"));
-        Assertions.assertEquals(new Response(OK), actualResponse);
+        assertEquals(new Response(OK, null, headers), actualResponse);
     }
 
     @Test
-    public void givenFileExistsAndGetMethod_shouldRespondOkWithBody() throws Exception {
+    public void whenFileExistsAndGetMethod_shouldRespondOkWithBody() throws Exception {
         Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1"));
-        byte[] body = Files.readAllBytes(Paths.get(rootPath + "/index.html"));
-        Assertions.assertEquals(new Response(OK, body), actualResponse);
+        assertEquals(new Response(OK, body, headers), actualResponse);
     }
 
-    private Request request(String content) throws RequestParseException {
+    @Test
+    public void whenPathIsDirectory_shouldRespondNotFound() throws Exception {
+        Response actualResponse = handler.handle(request("GET / HTTP/1.1"));
+        assertEquals(new Response(NOT_FOUND), actualResponse);
+    }
+
+    private Request request(String content) throws Exception {
         return new Request(new ByteArrayInputStream(content.getBytes()));
     }
 }
