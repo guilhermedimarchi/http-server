dbd8ee24d37927de03d73ded8b60b0ec1ed94755
guilhermedimarchi <guilherme.dimarchi@outlook.com>
2021-04-25 15:24:26 +0200

    refactoring file explorer html

---
+++ src/main/java/com/gui/http/handlers/FileExplorerHtml.java
@@ -0,0 +1,84 @@
+package com.gui.http.handlers;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileExplorerHtml {
+
+    private final String html;
+
+    public FileExplorerHtml(File file, String rootPath) throws IOException {
+        StringBuilder html = new StringBuilder();
+        String pathWithoutRoot = file.getPath().replace(rootPath, "");
+        html.append("<html>");
+        html.append("<head><title>Index of .");
+        html.append(pathWithoutRoot);
+        html.append("</title>");
+        html.append("</head>");
+        html.append("<body>");
+        html.append("<h1>Index of .");
+        html.append(pathWithoutRoot);
+        html.append("</h1>");
+        html.append("<pre>Name | Last modified | Size</pre><hr/>");
+
+        Path path = Paths.get(file.getPath());
+        if (path.getNameCount() > 1) {
+            if (path.getNameCount() == 2) {
+                html.append("<pre><a href=\"../\">../</a>\n");
+            } else {
+                html.append("<pre><a href=\"");
+                html.append(path.getParent().toString().replace(rootPath, ""));
+                html.append("\">../</a>\n");
+            }
+        }
+
+        for (File nestedFile : file.listFiles()) {
+            String filePath = nestedFile.getPath().replace(rootPath, "");
+            BasicFileAttributes attr = Files.readAttributes(nestedFile.toPath(), BasicFileAttributes.class);
+            html.append("<pre><a href=\"");
+            html.append(filePath);
+            html.append("\">");
+            html.append(nestedFile.getName());
+            html.append("</a> | ");
+            html.append(attr.lastModifiedTime());
+            html.append(" | ");
+            if (nestedFile.isDirectory()) {
+                html.append(folderSize(nestedFile));
+            } else {
+                html.append(attr.size());
+            }
+            html.append(" bytes");
+            html.append("</pre>");
+        }
+        html.append("</body>");
+        html.append("</html>");
+        this.html = html.toString();
+    }
+
+    public byte[] getHtmlBytes() {
+        return html.getBytes();
+    }
+
+    private long folderSize(File directory) throws IOException {
+        FileVisitor total = new FileVisitor();
+        Files.walkFileTree(directory.toPath(), total);
+        return total.getSize();
+    }
+
+    private class FileVisitor extends SimpleFileVisitor<Path> {
+        private long size;
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+            this.size += attrs.size();
+            return FileVisitResult.CONTINUE;
+        }
+
+        public long getSize() {
+            return size;
+        }
+    }
+
+}
--- src/main/java/com/gui/http/handlers/StaticHandler.java
+++ src/main/java/com/gui/http/handlers/StaticHandler.java
@@ -5,9 +5,8 @@ import com.gui.http.models.Response;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Date;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +15,7 @@ import static com.gui.http.HttpStatus.*;
 
 public class StaticHandler implements HttpHandler {
 
-    private String rootPath;
+    private final String rootPath;
 
     public StaticHandler(String rootPath) {
         this.rootPath = rootPath;
@@ -31,11 +30,11 @@ public class StaticHandler implements HttpHandler {
         if (!file.exists())
             return new Response(NOT_FOUND);
 
-
         byte[] body;
         Map<String, String> headers = new HashMap<>();
         if (file.isDirectory()) {
-            body = fileExplorerHtml(file);
+            FileExplorerHtml html = new FileExplorerHtml(file, rootPath);
+            body = html.getHtmlBytes();
             headers.put("Content-Type", "text/html");
         } else {
             body = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
@@ -49,73 +48,8 @@ public class StaticHandler implements HttpHandler {
             return new Response(OK, body, headers);
     }
 
-    private byte[] fileExplorerHtml(File file) throws IOException {
-        StringBuilder html = new StringBuilder();
-        html.append("<html>");
-        html.append("<head><title>Index of ");
-        html.append(file.getPath().replace(rootPath, ""));
-        html.append("</title>");
-        html.append("</head>");
-        html.append("<body>");
-        html.append("<h1>Index of ");
-        html.append(file.getPath());
-        html.append("</h1>");
-        html.append("<pre>Name | Last modified | Size</pre><hr/>");
-
-        Path path = Paths.get(file.getPath());
-        if (path.getNameCount() > 1) {
-            if (path.getNameCount() == 2) {
-                html.append("<pre><a href=\"../\">../</a>\n");
-            } else {
-                html.append("<pre><a href=\"");
-                html.append(path.getParent().toString().replace(rootPath, ""));
-                html.append("\">../</a>\n");
-            }
-        }
-
-        for (File nestedFile : file.listFiles()) {
-            String filePath = nestedFile.getPath().replace(rootPath, "");
-            BasicFileAttributes attr = Files.readAttributes(nestedFile.toPath(), BasicFileAttributes.class);
-            html.append("<pre><a href=\"");
-            html.append(filePath);
-            html.append("\">");
-            html.append(nestedFile.getName());
-            html.append("</a> | ");
-            html.append(attr.lastModifiedTime());
-            html.append(" | ");
-            if (nestedFile.isDirectory()) {
-                html.append(folderSize(nestedFile));
-            }else {
-                html.append(attr.size());
-            }
-            html.append(" bytes");
-            html.append("</pre>");
-        }
-        html.append("</body>");
-        html.append("</html>");
-        return html.toString().getBytes();
-    }
-
-    private long folderSize(File directory) throws IOException {
-        FileVisitor total = new FileVisitor();
-        Files.walkFileTree(directory.toPath(), total);
-        return total.getSize();
-    }
-
     private boolean methodNotImplemented(Request request) {
         return !"GET".equals(request.getMethod()) && !"HEAD".equals(request.getMethod());
     }
 
-    private class FileVisitor extends SimpleFileVisitor<Path> {
-        private long size;
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-            this.size += attrs.size();
-            return FileVisitResult.CONTINUE;
-        }
-        public long getSize() {
-            return size;
-        }
-    }
 }
