cbb6af738a5b3503a33aeb09f9ad7e9e9344b2bc
guilhermedimarchi <guilherme.dimarchi@outlook.com>
2021-04-26 17:52:01 +0200

    refactoring statif handler tests for better readability

--- src/test/java/com/gui/http/handlers/StaticHandlerTest.java
+++ src/test/java/com/gui/http/handlers/StaticHandlerTest.java
@@ -45,7 +45,7 @@ public class StaticHandlerTest {
 
     @Nested
     @TestInstance(Lifecycle.PER_CLASS)
-    class GivenPathIsFile {
+    class GivenPathToFile {
 
         private byte[] body;
         private Map<String, String> headers;
@@ -93,7 +93,7 @@ public class StaticHandlerTest {
     }
 
     @Nested
-    class GivenPathIsDirectory {
+    class GivenPathToDirectory {
         private OutputStream out;
 
         @BeforeEach
@@ -152,58 +152,68 @@ public class StaticHandlerTest {
             output = new ByteArrayOutputStream();
         }
 
-        @Test
-        public void whenIfNoneMatchIsEqualEtag_shouldRespondNotModified() throws Exception {
-            handler.handle(request("GET /index.html HTTP/1.1")).send(output);
-            String etags = "etag1,etag2," + headerValueOf(ETAG, output.toString());
-            Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_NONE_MATCH + ":" + etags));
-            assertEquals(new Response(NOT_MODIFIED), actualResponse);
+        @Nested
+        class GivenIfNoneMatchHeader {
+            @Test
+            public void whenIsEqualEtag_shouldRespondNotModified() throws Exception {
+                handler.handle(request("GET /index.html HTTP/1.1")).send(output);
+                String etags = "etag1,etag2," + headerValueOf(ETAG, output.toString());
+                Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_NONE_MATCH + ":" + etags));
+                assertEquals(new Response(NOT_MODIFIED), actualResponse);
+            }
+
+            @Test
+            public void whenIsStar_shouldRespondNotModified() throws Exception {
+                Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_NONE_MATCH + ":*"));
+                assertEquals(new Response(NOT_MODIFIED), actualResponse);
+            }
         }
 
-        @Test
-        public void whenIfNoneMatchIsStar_shouldRespondNotModified() throws Exception {
-            Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_NONE_MATCH + ":*"));
-            assertEquals(new Response(NOT_MODIFIED), actualResponse);
+        @Nested
+        class GivenIfMatchHeader {
+            @Test
+            public void whenIsDifferentThenEtag_shouldRespondPreconditionFailed() throws Exception {
+                handler.handle(request("GET /index.html HTTP/1.1")).send(output);
+                String etag = "randometag";
+                Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MATCH + ":" + etag));
+                assertEquals(new Response(PRECONDITION_FAILED), actualResponse);
+            }
+
+            @Test
+            public void whenIsEqualToEtag_shouldProceedWithRequest() throws Exception {
+                handler.handle(request("GET /index.html HTTP/1.1")).send(output);
+                String etags = "etag1,etag2," + headerValueOf(ETAG, output.toString());
+                Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MATCH + ":" + etags));
+                assertNotEquals(new Response(PRECONDITION_FAILED), actualResponse);
+            }
+
+            @Test
+            public void whenIsStar_shouldProceedWithRequest() throws Exception {
+                Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MATCH + ": *"));
+                assertNotEquals(new Response(PRECONDITION_FAILED), actualResponse);
+            }
         }
 
-        @Test
-        public void whenIfMatchIsDifferentThenEtag_shouldRespondPreconditionFailed() throws Exception {
-            handler.handle(request("GET /index.html HTTP/1.1")).send(output);
-            String etag = "randometag";
-            Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MATCH + ":" + etag));
-            assertEquals(new Response(PRECONDITION_FAILED), actualResponse);
-        }
+        @Nested
+        class GivenIfModifiedSinceHeader {
 
-        @Test
-        public void whenIfMatchIsEqualToEtag_shouldProceedWithRequest() throws Exception {
-            handler.handle(request("GET /index.html HTTP/1.1")).send(output);
-            String etags = "etag1,etag2," + headerValueOf(ETAG, output.toString());
-            Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MATCH + ":" + etags));
-            assertNotEquals(new Response(PRECONDITION_FAILED), actualResponse);
-        }
+            @Test
+            public void whenResourceIsSameDate_shouldRespondNotModified() throws Exception {
+                handler.handle(request("GET /index.html HTTP/1.1")).send(output);
+                Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MODIFIED_SINCE + ":" + headerValueOf(LAST_MODIFIED, output.toString())));
+                assertEquals(new Response(NOT_MODIFIED), actualResponse);
+            }
 
-        @Test
-        public void whenIfMatchIsStar_shouldProceedWithRequest() throws Exception {
-            Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MATCH + ": *"));
-            assertNotEquals(new Response(PRECONDITION_FAILED), actualResponse);
-        }
+            @Test
+            public void whenResourceIsNewer_shouldProceedWithRequest() throws Exception {
+                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(HTTP_DATE_FORMAT);
 
-        @Test
-        public void whenIfModifiedSince_shouldRespondNotModified() throws Exception {
-            handler.handle(request("GET /index.html HTTP/1.1")).send(output);
-            Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MODIFIED_SINCE + ":" + headerValueOf(LAST_MODIFIED, output.toString())));
-            assertEquals(new Response(NOT_MODIFIED), actualResponse);
-        }
+                handler.handle(request("GET /index.html HTTP/1.1")).send(output);
+                ZonedDateTime date = ZonedDateTime.parse(headerValueOf(LAST_MODIFIED, output.toString()), dateFormatter).minusDays(1);
 
-        @Test
-        public void whenResourceIsNewerThenClientCache_shouldProceedWithRequest() throws Exception {
-            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(HTTP_DATE_FORMAT);
-
-            handler.handle(request("GET /index.html HTTP/1.1")).send(output);
-            ZonedDateTime date = ZonedDateTime.parse(headerValueOf(LAST_MODIFIED, output.toString()), dateFormatter).minusDays(1);
-
-            Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MODIFIED_SINCE + ":" + dateFormatter.format(date)));
-            assertNotEquals(new Response(NOT_MODIFIED), actualResponse);
+                Response actualResponse = handler.handle(request("GET /index.html HTTP/1.1\n" + IF_MODIFIED_SINCE + ":" + dateFormatter.format(date)));
+                assertNotEquals(new Response(NOT_MODIFIED), actualResponse);
+            }
         }
 
         private String headerValueOf(String header, String response) {
@@ -214,5 +224,6 @@ public class StaticHandlerTest {
             }
             return "";
         }
+
     }
 }
