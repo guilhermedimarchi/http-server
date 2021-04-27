79783c792121fe6b1bbc9cb838931d12836cc7b5
guilhermedimarchi <guilherme.dimarchi@outlook.com>
2021-04-27 21:36:34 +0200

    breaking logic into different methods

--- src/main/java/com/gui/http/ClientSocketManager.java
+++ src/main/java/com/gui/http/ClientSocketManager.java
@@ -35,38 +35,43 @@ public class ClientSocketManager implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
-             BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+             BufferedOutputStream output = new BufferedOutputStream(socket.getOutputStream());
              BufferedReader input = new BufferedReader(new InputStreamReader(in, US_ASCII))) {
 
-            boolean connected = true;
-            while (connected) {
-                requestCount++;
-                Response response;
-                try {
-                    LOGGER.debug("request received from: " + socket);
-                    Request request = new Request(input);
-                    response = handler.handle(request);
-                    connected = processPersistentConnection(response, request);
-                } catch (RequestParseException e) {
-                    LOGGER.error("bad request", e);
-                    connected = false;
-                    response = new Response(BAD_REQUEST, e.getMessage().getBytes());
-                } catch (Exception e) {
-                    LOGGER.error("internal server error", e);
-                    connected = false;
-                    response = new Response(INTERNAL_SERVER_ERROR);
-                }
-                response.send(out);
-            }
+            processConnection(input, output);
+
         } catch (IOException e) {
             LOGGER.error("error writing response to socket output", e);
         } finally {
             try {
                 socket.close();
-                LOGGER.info("closing connection: " + socket);
+                LOGGER.info("closed connection: " + socket);
             } catch (IOException e) {
                 LOGGER.error("could not close socket", e);
             }
+        }
+    }
+
+    private void processConnection(BufferedReader in, BufferedOutputStream out) throws IOException {
+        boolean connected = true;
+        while (connected) {
+            requestCount++;
+            Response response;
+            try {
+                LOGGER.debug("request received from: " + socket);
+                Request request = new Request(in);
+                response = handler.handle(request);
+                connected = processPersistentConnection(response, request);
+            } catch (RequestParseException e) {
+                LOGGER.error("bad request", e);
+                connected = false;
+                response = new Response(BAD_REQUEST, e.getMessage().getBytes());
+            } catch (Exception e) {
+                LOGGER.error("internal server error", e);
+                connected = false;
+                response = new Response(INTERNAL_SERVER_ERROR);
+            }
+            response.send(out);
         }
     }
 
